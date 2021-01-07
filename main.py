1786cb7ca021c8d072e20138f58b59f7365bcf47
tomassk10a <tomass.k04@gmail.com>
2021-01-07 08:52:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Sveika pasaule!")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "Pitons")
+print(f"Ja saskaitisim 5 un 7 tad bus {5+7}.")
+print("Sveika, "+" pasaule") #komentars
+print("Tomass")
+print("Tomass")
+print("Tomass")
+print("Tomass")
+print("Tomass")
+print("Tomass")
+print("Tomass "*7)
